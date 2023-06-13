043e4c118e4b2706578094ae953afc3675275a22
Yoshitha Peddireddy <yoshee257@gmail.com>
2023-06-13 14:56:09 +0530

    last

--- config/db.config.js
+++ config/db.config.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: PROCESS.env.HOST,
-    user: PROCESS.env.USER,
-    password:  PROCESS.env.PASSWORD,
-    database: PROCESS.env.DATABASE
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 });
 
 module.exports = pool;
